ebe2b0f561ea57fd5e0bda8b0a45ec9efc65a1af
senektra <rcvallada@gmail.com>
2024-04-20 21:28:55 -0700

    Add middleware

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 import express from 'express'
 import * as Sentry from '@sentry/node'
 import config from './utils/config.js'
+import mongoose from 'mongoose'
+import middleware from './utils/middleware.js'
+
+mongoose.connect(config.mongodbUri)
+
+// Use morgan logger for requests
+app.use(middleware.morganLogger)
 
 const app = express()
+app.use(express.json())
+app.use(cors())
 
 if (config.live) {
   Sentry.init(config.SentryOptions(app))
@@ -10,7 +19,12 @@ if (config.live) {
   app.use(Sentry.Handlers.tracingHandler())
 }
 
+// app.use('/api/blogs', blogsRouter)
+
 if (config.live)
   app.use(Sentry.Handlers.errorHandler())
+
+app.use(middleware.nonApiErrorHandler)
+app.use(middleware.errorHandler)
 
 export default app
